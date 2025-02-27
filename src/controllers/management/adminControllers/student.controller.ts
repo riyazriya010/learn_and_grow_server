540be471b4f67803aec04738f959f27a3a914949
riyazriya010 <riyur017@gmail.com.com>
2025-02-27 11:19:21 +0530

    cookie updated for admin

--- src/controllers/management/adminControllers/student.controller.ts
+++ src/controllers/management/adminControllers/student.controller.ts
@@ -28,9 +28,15 @@ export default class AdminStudentController {
             return res
                 .status(200)
                 .cookie('accessToken', userJwtToken, {
-                    httpOnly: false
+                    httpOnly: false,
+                    secure: true,
+                    sameSite: "strict",
+                    domain: '.learngrow.live'
                 }).cookie('refreshToken', userRefreshToken, {
-                    httpOnly: true
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: "strict",
+                    domain: '.learngrow.live'
                 })
                 .send({
                     success: true,
